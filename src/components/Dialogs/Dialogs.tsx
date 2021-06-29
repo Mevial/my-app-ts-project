18a36ea730ffb7beaf67382e6d0acf368520f504
Max <61512470+Mevial@users.noreply.github.com>
2021-06-29 17:15:31 +0300

    lesson 21. add add <div ClassName={styles.dialog + ' ' + styles.active}

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,15 +2,36 @@ import React from "react";
 import styles from './Dialogs.module.css'
 
 type DialogsPropsType = {
-dialogsValue: string
+    dialogsValue: string
 }
 
 const Dialogs: React.FC<DialogsPropsType> = (props) => {
     return (
-        <div className={styles.content}>
-            {props.dialogsValue}
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+            <div className={styles.dialog + ' ' + styles.active}>
+                {props.dialogsValue}
+            </div>
+            <div className={styles.dialog}>
+                {props.dialogsValue}
+            </div>
+            <div className={styles.dialog}>
+                {props.dialogsValue}
+            </div>
+            <div className={styles.dialog}>
+                {props.dialogsValue}
+            </div>
+            <div className={styles.dialog}>
+                {props.dialogsValue}
+            </div>
         </div>
-    )
+    <div className={styles.messages}>
+        <div className={styles.dialog}>Hi</div>
+        <div className={styles.dialog}>How are you</div>
+        <div className={styles.dialog}>How Yo you</div>
+    </div>
+</div>
+)
 }
 
 export default Dialogs
